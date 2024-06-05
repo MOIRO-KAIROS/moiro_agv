4eb586460358d70b18a97de62a42ef6bf0162939
Mincheol710313 <mincheol710313@gmail.com>
2024-06-05 11:22:57 +0900

    migrating description from moiro_description to moiro_agv

---
+++ moiro_agv_bringup/launch/moiro_agv_state_publisher.launch.py
@@ -0,0 +1,48 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+import xacro
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('moiro_agv_description')
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'moiro_agv.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='True'
+    )
+
+    show_gui = LaunchConfiguration('gui')
+
+    return LaunchDescription([
+        gui_arg,
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation clock if true'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[
+                {'use_sim_time': use_sim_time},
+                {'robot_description': robot_urdf}
+            ]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
+        ),
+    ])
